9e84382cf3a9fc05d1c09159318c9ffd7a519f34
Shukurulla <shukurullatursinbayev978@gmail.com>
2025-08-18 16:20:23 +0500

    start

--- src/pages/Templates.jsx
+++ src/pages/Templates.jsx
@@ -19,6 +19,7 @@ const Templates = () => {
     { code: "uz", name: "O'zbekcha", flag: "🇺🇿" },
     { code: "ru", name: "Русский", flag: "🇷🇺" },
     { code: "uz_kiril", name: "Ўзбекча", flag: "🇺🇿" },
+    { code: "kaa", name: "Qaraqalpaqsha", flag: "kaa" },
   ];
 
   useEffect(() => {
